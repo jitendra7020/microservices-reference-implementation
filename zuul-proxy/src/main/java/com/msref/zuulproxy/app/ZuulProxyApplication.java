8f83ced8f7ab86e827af8f3f85e8b4ed3f7f7130
Thilanka Sarindu Udagepala <sudagepala@Virtusa.com>
2018-12-03 14:43:14 +0530

    adding zuul filters

--- zuul-proxy/src/main/java/com/msref/zuulproxy/app/ZuulProxyApplication.java
+++ zuul-proxy/src/main/java/com/msref/zuulproxy/app/ZuulProxyApplication.java
@@ -3,8 +3,10 @@ package com.msref.zuulproxy.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.msref.zuulproxy.filters") //This is to initialize the filters
 @EnableZuulProxy
 public class ZuulProxyApplication {
 
---
+++ zuul-proxy/src/main/java/com/msref/zuulproxy/filters/route/RoutingInfoLogRouteFilter.java
@@ -0,0 +1,44 @@
+package com.msref.zuulproxy.filters.route;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import com.msref.zuulproxy.filters.error.ErrorInfoLogErrorFilter;
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+
+@Component
+public class RoutingInfoLogRouteFilter extends ZuulFilter {
+	
+	private static Logger logger = LoggerFactory.getLogger(ErrorInfoLogErrorFilter.class);
+
+	@Override
+	public Object run() throws ZuulException {
+		RequestContext requestContext = RequestContext.getCurrentContext();
+		HttpServletRequest request = requestContext.getRequest();
+		
+		logger.info("Route Filter: " + String.format("%s request to %s", request.getMethod(), request.getRequestURL().toString()));		
+		
+		return null;
+	}
+
+	@Override
+	public boolean shouldFilter() {
+		return true;
+	}
+
+	@Override
+	public int filterOrder() {
+		return 1;
+	}
+
+	@Override
+	public String filterType() {
+		return "route";
+	}
+
+}
